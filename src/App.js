5f58b426b91fed80b35e1ce5db9fd70ee395212c
ISAACHERNANDEZ <seudr@comunidad.unam.mx>
2023-11-14 15:33:53 -0600

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function ContentAnalysis() {
   return (
     <div>
       <h1>Content Analysis Tool</h1>
-      <input type="text" placeholder="Enter a URL" />
+      <input type="text" placeholder="Enter a URL" id="User_input"/>
       <button onClick={analyzeButtonClick}>Analyze</button>
       <button onClick={generateButtonClick}>Generate</button>
     </div>
@@ -14,11 +14,29 @@ function ContentAnalysis() {
 const analyzeButtonClick = () => {
   // Code to analyze the entered URL goes here
   console.log("Analyzing...");
+  VisionServiceOptions
+  VisionSource
 };
 
 const generateButtonClick = () => {
   // Code to generate content based on the analysis goes here
   console.log("Generating...");
 };
+
+const VisionServiceOptions = () => {
+  serviceOptions = new VisionServiceOptions(
+  new URL(System.getenv("VISION_ENDPOINT")),
+  System.getenv("VISION_KEY"));
+}
+
+function othername() {
+  var input = document.getElementById("User_input").value;
+  alert(input);
+}
+
+const VisionSource = () => {
+  imageSource = VisionSource.fromUrl(
+      new URL(othername()));
+}
 
 export default ContentAnalysis;
